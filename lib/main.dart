3b5a0a05d9b9276db2a648323435fd352f72ae8b
Keshav Sharma <keshav2111101@akgec.ac.in>
2023-05-13 16:23:20 +0530

    chapter 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
@@ -17,7 +18,10 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
-      home: const Homepage(),
+      home: ApiProvider(
+        api: Api(),
+        child: const Homepage(),
+      ),
     );
   }
 }
@@ -30,28 +34,76 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  String title = 'HomePage';
+  ValueKey _textkey = const ValueKey<String?>(null);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(
-          child: Text(title),
+          child: Text(ApiProvider.of(context).api.dateandTime ?? 'HomePage'),
         ),
       ),
       body: GestureDetector(
-        onTap: () {
+        onTap: () async {
+          final api = ApiProvider.of(context).api;
+          final dateAndTime = await api.getDateandTime();
           setState(() {
-            title = DateTime.now().hour.toString() +
-                '-' +
-                DateTime.now().minute.toString() +
-                '-' +
-                DateTime.now().second.toString() +
-                '.' +
-                DateTime.now().millisecond.toString();
+            _textkey = ValueKey(dateAndTime);
           });
         },
+        child: SizedBox.expand(
+          child: Center(
+            child: DateandTime(
+              key: _textkey,
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
+
+class Api {
+  String? dateandTime;
+
+  Future<String> getDateandTime() async {
+    return Future.delayed(
+            const Duration(seconds: 1),
+            () => dateandTime =
+                '${DateTime.now().hour}-${DateTime.now().minute}-${DateTime.now().second}.${DateTime.now().millisecond}')
+        .then((value) {
+      dateandTime = value;
+      return value;
+    });
+  }
+}
+
+class ApiProvider extends InheritedWidget {
+  final Api api;
+  final String uuid;
+
+  ApiProvider({
+    Key? key,
+    required this.api,
+    required Widget child,
+  })  : uuid = const Uuid().v4(),
+        super(key: key, child: child);
+  @override
+  bool updateShouldNotify(covariant ApiProvider oldWidget) {
+    return uuid != oldWidget.uuid;
+  }
+
+  static ApiProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ApiProvider>()!;
+  }
+}
+
+class DateandTime extends StatelessWidget {
+  const DateandTime({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final api = ApiProvider.of(context).api;
+    return Text(api.dateandTime ?? 'Tap on the Mee');
   }
 }
